c0690c34528544873415f8cc0b00649e84c5e10d
Toukir <toukerahmed.2728@gmail.com>
2025-08-13 22:53:27 +0600

    //21-2 Who is the tallest? Find the max number in an array

--- aviiiiiiiii_module21.js
+++ aviiiiiiiii_module21.js
@@ -1,33 +1,53 @@
 //21-1 Can you find out who will get the Strawberry?
- //max of 2
- function maxTwo(a,b){
-    if(a>b){
-        return a;
-    }
-    else{
-        return b;
-    }
- }
- console.log(maxTwo(45,90));
+//max of 2
+function maxTwo(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+console.log(maxTwo(45, 90));
 
- //max of three
+//max of three
 
- function maxofTree(a,b,c){
-    if(a>b && a>c){
-        return a;
+function maxofTree(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+console.log(maxofTree(7, 89, 2));
+
+// max of multiple number
+function maxNumber(arr) {
+  return Math.max(...arr);
+}
+console.log(maxNumber([7, 90, 34, 21, 2, 12, 56, 89, 555, 34]));
+
+//21-2 Who is the tallest? Find the max number in an array
+function findTallest(arr) {
+  let max = arr[0];
+  for (let i of arr) {
+    if (i > max) {
+      max = i;
     }
-    else if(b>a && b>c){
-        return b;
-    }
-    else{
-        return c;
-    }
- }
- console.log(maxofTree(7,89,2));
+  }
+  return max;
+}
+console.log(findTallest([9, 8, 90, 5, 67, 54]));
 
 
- // max of multiple number
- function maxNumber(arr){
-    return Math.max(...arr);
- }
- console.log(maxNumber([7,90,34,21,2,12,56,89,555,34]))
+function findSmallest(arr) {
+  let min = arr[0];
+  for (let i of arr) {
+    if (i < min) {
+      min = i;
+    }
+  }
+  return min;
+}
+console.log(findSmallest([9, 8, 90, 5, 67, 54]));
